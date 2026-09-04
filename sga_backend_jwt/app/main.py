018b27968c9b7b0437390b3f92bf4439e25bc748
gabielrodriguez <franciscogabrielrodriguez7-tech>
2026-09-03 21:15:16 -0500

    fix: agregar configuracion de CORS para frontend en Vercel

--- sga_backend_jwt/app/main.py
+++ sga_backend_jwt/app/main.py
@@ -74,7 +74,8 @@ app.add_middleware(
 
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://sga-three-kappa.vercel.app"
     ],
 
     allow_credentials=True,
